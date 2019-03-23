628dac86b3e28cf2e5e88c8ca85670c257bd5398
J Dhar <jasmine.k.bhambra@gmail.com>
2019-03-23 10:11:36 -0400

    added bot code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ChatBot from 'react-simple-chatbot';
+
+const steps = [
+  {
+    id: '0',
+    message: 'Welcome to react chatbot!',
+    trigger: '1',
+  },
+  {
+    id: '1',
+    message: 'Bye!',
+    end: true,
+  },
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+         <ChatBot steps={steps} />
       </div>
     );
   }
