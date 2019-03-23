04bdc1f9f2ac459711ef8316681aeab000efbd2e
J Dhar <jasmine.k.bhambra@gmail.com>
2019-03-23 13:00:19 -0400

    just some color changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,88 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import dogpic from './resources/doggo.jpeg'
 import './App.css';
 import ChatBot from 'react-simple-chatbot';
+import styled from 'styled-components';
+
+const theme = styled.div`
+  background: '#f8faf4',
+  fontFamily: 'Helvetica Neue',
+  headerBgColor: '#EF6C00',
+  headerFontColor: '#fff',
+  headerFontSize: '55px',
+  botBubbleColor: '#EF6C00',
+  botFontColor: '#fff',
+  userBubbleColor: '#fff',
+  userFontColor: '#4a4a4a',
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
 
 const steps = [
-  {
-    id: '0',
-    message: 'Welcome to react chatbot!',
-    trigger: '1',
-  },
-  {
-    id: '1',
-    message: 'Bye!',
-    end: true,
-  },
-];
+    {
+      id: '1',
+      message: 'What is your name?',
+      trigger: '2',
+    },
+    {
+      id: '2',
+      user: true,
+      trigger: '3',
+    },
+    {
+      id: '3',
+      message: 'Hi {previousValue}, nice to meet you!',
+      end: true,
+    },
+  ];
 
-class App extends Component {
+class Hackers extends Component {
   render() {
     return (
-      <div className="App">
-         <ChatBot steps={steps} />
+      <div>
+      <p>{this.props.hackers}</p>
+      <button onClick={this.props.clickHandler}>+1</button>
       </div>
+      );
+  }
+}
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      hackers: 999,
+    }
+  }
+
+buttonClick = (event) => {
+  this.setState({
+    hackers: this.state.hackers + 1,
+  });
+}
+
+
+  render() {
+    return (
+    <div>
+    <Hackers
+    hackers = {this.state.hackers}
+    clickHandler = {this.buttonClick}
+    />
+    <styled theme={theme}>
+    <div>
+    <Wrapper>
+    <div>
+      <ChatBot steps={steps} />
+      </div>
+      </Wrapper>
+      </div>
+    </styled>
+    </div>
     );
   }
 }
